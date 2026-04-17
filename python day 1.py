49089c2b66846419e081f5bb51701b2477f8743c
DeepakporikGitHub <116652583+DeepakporikGitHub@users.noreply.github.com>
2026-04-17 15:47:11 +0530

    Add files via upload

---
+++ python day 1.py
@@ -0,0 +1,34 @@
+Python 3.14.4 (tags/v3.14.4:23116f9, Apr  7 2026, 14:10:54) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> print("hello deepak, i started my journey")
+hello deepak, i started my journey
+>>> #1. Add two numbers
+>>> a=1
+>>> b=4
+>>> print('a+b')
+a+b
+>>> print(a+b)
+5
+>>> 
+>>> 
+>>> #2. Take user Input
+>>> name= input("enter your name: ")
+enter your name: DEEPAK
+>>> print("welcome",name)
+welcome DEEPAK
+>>> 
+>>> #3. Simple Calculator
+>>> x=int(input('enter firt number:'))
+enter firt number:
+Traceback (most recent call last):
+  File "<pyshell#13>", line 1, in <module>
+    x=int(input('enter firt number:'))
+ValueError: invalid literal for int() with base 10: ''
+>>> #3.Simple Calculator
+>>> x=int(input("enter firt number:"))
+enter firt number:2
+>>> y=int(input("enter second number:"))
+enter second number:2
+>>> print("Sum is:", x + y)
+Sum is: 4
+>>> 
